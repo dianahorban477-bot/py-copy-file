0c4967ef1093f92cc39ea6b200992905c7661946
dianahorban <dianahorban477@gmail.com>
2026-04-28 13:45:06 +0200

    The solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None | bool:
+    words = command.split()
+    if len(words) != 3:
+        return None
+    try:
+        open(words[1], "r")
+    except FileNotFoundError:
+        return None
+    if words[1] == words[2]:
+        return None
+    elif words[0] != "cp":
+        return None
+    else:
+        with open(words[1], "r") as f1, open(words[2], "w") as f2:
+            f2.write(f1.read())
+            return True
